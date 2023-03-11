dda108f14f4c8a555ddd0d05dcc6a9177bbd80e4
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-03-11 21:29:07 +0800

    :construction_worker: Add latexindent

--- scripts/generate-template.tex.pl
+++ scripts/generate-template.tex.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/env -S perl -0777 -p
-s/(?<=\\multicolumn\{2\}\{\|p\{\\linewidth\}\|\}\{%\n)(.|\n)*?(?=  \}\\\\)//g
+s/(?<=\\multicolumn\{2\}\{\|p\{\\linewidth\}\|\}\{%\n)(.|\n)*?(?=  \}\s*\\\\)//g
